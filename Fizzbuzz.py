c2405136e40cf62fdd0f9a71e3d1cdb32b583354
RoboZe <roboticsxzero@gmail.com>
2024-12-28 13:39:48 +0530

    "Comment added"

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,4 +1,10 @@
 
+
+# Fizz Buzz Problem  : In this Print number from 1 to 100 however when the number will be divisible by 3
+# print fizz and when number is divisible 5 print buzz
+
+
+
 for i in range(1, 101):
     output = ""
     if i % 3 == 0:
