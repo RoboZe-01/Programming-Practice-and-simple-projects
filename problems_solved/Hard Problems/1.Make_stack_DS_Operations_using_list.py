a1118669cdc2645122b914787605c065dfaba6d4
RoboZe <roboticsxzero@gmail.com>
2025-01-09 11:01:27 +0530

    commit

---
+++ problems_solved/Hard Problems/1.Make_stack_DS_Operations_using_list.py
@@ -0,0 +1,26 @@
+
+
+
+# Stacks: Implement a stack data structure using a list in Python. 
+# Include methods for push, pop, and peek operations.
+
+
+
+
+# Basically we want to make a python program to make a functios to perform: 
+# 1.Push  ( Add the objest to top )
+# 2.pop   ( removes or return the top element)
+# 3.Peek (Returns the top element without removing them )
+# 4. isEmpty ( Check is list is empty or not )
+
+
+class Stack :
+    def __init__(self):
+        self.items = []
+
+    def push (self,item):
+        return self.items.append(item)
+    def pop (self,item):
+        
+
+    
--- problems_solved/Moderate Problems/1.dict_problem.py
+++ problems_solved/Moderate Problems/1.dict_problem.py
@@ -7,15 +7,18 @@
 
 # Make the dictonary Content sudent name and roll number 
 
-student_dict = {13 : "Subha" , 7 :"Prem",15 :"Rahul",8 : "Om"}
+student_dict = {13 : "Subha" , 7 :"Prem",15 :"Rahul",8 : "Om"}  
+
+# TO prevent this 
 while True :
-            try :  
-             user_input = int(input("ENter the roll number : "))
-             break
-            except :
-             print("invalid roll no.")
+    try :
+        user_input = int(input("Enter the roll number : "))
+        break
+    except :
+            print("invalid roll number ")
 
 def get_student_name() :
                 name = student_dict.get(user_input)
                 print(name)
+                
 get_student_name()
