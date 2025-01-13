e59a082f89cfa8b5517c1c4e8fc0a4e6dbc86ef4
RoboZe <roboticsxzero@gmail.com>
2025-01-13 21:31:28 +0530

    yes

---
+++ problems_solved/Simple Level Problems/easy_.py
@@ -0,0 +1,15 @@
+
+
+
+
+# THis is the easy problem 
+while True:
+
+        user_input = (input("Do You love me !! (Enter y/n )  : "))
+
+        if user_input.lower() == "y" : 
+            print(" I love you too much  !! ")
+            
+
+        else :
+            print("Fuck off then !! ")    
