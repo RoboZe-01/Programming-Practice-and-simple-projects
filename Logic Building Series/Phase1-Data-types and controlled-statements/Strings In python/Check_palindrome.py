2c882b012bf2df1eb7dd2e82bf04c6277d2f4d4f
RoboZe <roboticsxzero@gmail.com>
2025-08-03 00:46:22 +0530

    2nd problem solved

---
+++ Logic Building Series/Phase1-Data-types and controlled-statements/Strings In python/Check_palindrome.py
@@ -0,0 +1,25 @@
+
+
+# creating the function to give palindrome
+
+# Palindrome : When the reverse of the string is same as normal
+
+# Reusing the function created in the last problem
+
+def reverse_str (x):          # Function defination
+    return x[::-1]            # returning the reverse string
+
+# a= "madam"  # using static varible 
+
+# Let's  do this same using dynamic varible 
+
+a = input("Enter the string to check Palindrome : ")
+
+while True:
+
+    if a == reverse_str(a):
+        print(" This is palindrome")
+    else:    
+        print(" This is not the Palindrome")
+
+
---
+++ Logic Building Series/Phase1-Data-types and controlled-statements/Strings In python/Reverse the string.py
@@ -0,0 +1,11 @@
+
+
+
+def reverse_str (x):          # Function defination
+    return x[::-1]            # returning the reverse string
+
+a = " This string is to test the function "
+b= reverse_str(a)
+print(b)
+
+# Output -->  noitcnuf eht tset ot si gnirts sihT 
