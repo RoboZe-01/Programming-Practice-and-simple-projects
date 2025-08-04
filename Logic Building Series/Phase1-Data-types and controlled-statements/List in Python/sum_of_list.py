3643668be31bfe42b246713edd98c8c22d0cb1b4
RoboZe <roboticsxzero@gmail.com>
2025-08-04 11:15:13 +0530

    Day 3 of logic building is started

---
+++ Logic Building Series/Phase1-Data-types and controlled-statements/List in Python/sum_of_list.py
@@ -0,0 +1,19 @@
+
+# Problem Statement : 
+
+# Sum of List
+
+# Given a list of numbers, return the sum.
+
+# Example: [1, 2, 3] → 6
+
+
+
+list1 = [1,2,3,4]
+
+def sum_list (list2):
+    sum = 0 
+    for lst in list1 :
+        sum = sum + lst
+
+    return sum    
